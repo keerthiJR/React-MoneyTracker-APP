abb866018bf656d9c97fd30ec4c4df9f3d3ba00c
keerthiJR <keerthigandhi1552001@gmail.com>
2022-09-27 00:05:26 +0530

    added cancel button with conditional content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css'
 const NewExpense = (props) => {
-    const saveExpenseDataHandler = (enteredExpenseData)=>{
+    const [isEditing, setIsEditing] = useState(false)
+    const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         }
         console.log(expenseData)
         props.onAddExpense(expenseData)
+        setIsEditing((check)=>{
+            return !check
+        })
+    }
+
+    const startEditingHandler = () =>{
+        setIsEditing((check)=>{
+            return !check
+        })
     }
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+            {!isEditing && <button onClick={startEditingHandler} >Add New Expense</button>}
+            {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={startEditingHandler}/>}
         </div>
     );
 }
